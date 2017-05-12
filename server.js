9410f83d43b24035683ae9d0c073cc7a22ca2776
Farid Jafarlee <placeboy.jafaroff@gmail.com>
2017-05-12 12:19:47 +0400

    made GET, GET ID

--- server.js
+++ server.js
@@ -4,9 +4,35 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false
+}, {
+    id: 2,
+    description: 'Go to market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Feed the cat',
+    completed: true
+}];
 
 app.get('/', function(req, res) {
     res.send('Todo API Root');
+});
+
+app.get('/todos', function(req, res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function (req, res) {
+    var matchedTodo;
+    todos.forEach(function (todo) {
+        if (todo.id === parseInt(req.params.id, 10)) matchedTodo = todo;
+    });
+    if (typeof matchedTodo !== 'undefined') res.json(matchedTodo);
+    else res.status('404').send();
 });
 
 app.listen(PORT, function () {
