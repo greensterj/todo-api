f38209b889beb1593bcdeeb95dbe418e44745b1a
Farid Jafarlee <placeboy.jafaroff@gmail.com>
2017-05-16 18:24:46 +0400

    db settings

--- db.js
+++ db.js
@@ -6,11 +6,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
 if (env === 'production') {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        logging: true
-    });
+    sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
     sequelize = new Sequelize(undefined, undefined, undefined, {
         'dialect': 'sqlite',
